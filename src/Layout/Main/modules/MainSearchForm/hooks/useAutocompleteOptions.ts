d4b6c7b3dfb719a2bb3f03a6d2c77071e6a9b0fd
Artem Zviahintsev <artmzvn@gmail.com>
2021-11-04 12:28:30 +0200

    Search fix

--- src/Layout/Main/modules/MainSearchForm/hooks/useAutocompleteOptions.ts
+++ src/Layout/Main/modules/MainSearchForm/hooks/useAutocompleteOptions.ts
@@ -4,6 +4,7 @@ import { SellerType } from '@/Types/SellerType';
 import { useCallback, useState } from 'react';
 
 export const useAutocompleteOptions = (setSeller: (seller?: SellerType) => void) => {
+  const [autocompleteExpression, setAutocompleteExpression] = useState('');
   const [sellerOptions, setSellerOptions] = useState<SellerType[]>([]);
   const [autocompleteOptions, setAutocompleteOptions] = useState<OptionType[]>([]);
 
@@ -17,8 +18,16 @@ export const useAutocompleteOptions = (setSeller: (seller?: SellerType) => void)
   ); // selectSeller
 
   const loadAutocompleteOptions = useCallback((value: string) => {
-    if (value.length === 3) {
-      AutocompleteApi.getAutocompleteList({ item: value }).then((response) => {
+    let seachExpression = '';
+    if(value.length > 2) {
+        seachExpression = value.slice(0, 3).toLowerCase();
+    } // if
+
+    if (autocompleteExpression.localeCompare(seachExpression) !== 0 && seachExpression.length === 3) {
+      setAutocompleteExpression(seachExpression);
+
+      AutocompleteApi.getAutocompleteList({ item: seachExpression })
+      .then((response) => {
         const suggestion_list = response.data.suggestion_list;
 
         setSellerOptions(suggestion_list);
@@ -28,9 +37,12 @@ export const useAutocompleteOptions = (setSeller: (seller?: SellerType) => void)
             value: item.seller_name,
           })),
         );
+      })
+      .catch(() => {
+        setAutocompleteExpression('');
       });
     } // if
-  }, []); // loadAutocompleteOptions
+  }, [autocompleteExpression]); // loadAutocompleteOptions
 
   return {
     selectSeller,
